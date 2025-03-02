2531a928828d89f145b22077a104eef708e4c88b
AbangAyoma <abangayoma@gmail.com>
2025-03-02 11:37:38 +0100

    feat: add the moon and sun icons

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,32 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import "./ThemeToggle.css";
 
 const ThemeToggle: React.FC = () => {
   const [darkMode, setDarkMode] = useState<boolean>(() => {
     return localStorage.getItem("theme") === "dark";
   });
 
+  const sun_icon = <i className="fas fa-sun"></i>;
+  const moon_icon = <i className="fas fa-moon"></i>;
+  
   useEffect(() => {
     const body = document.body;
-    const header = document.querySelector("header");
+    const search = document.querySelector(".search-container");
     const card = document.querySelector(".user-card");
 
     if (darkMode) {
       body.classList.add("dark-mode");
-      header?.classList.add("header-dark");
+      search?.classList.add("search-dark");
       card?.classList.add("card-dark");
       localStorage.setItem("theme", "dark");
     } else {
       body.classList.remove("dark-mode");
-      header?.classList.remove("header-dark");
+      search?.classList.remove("search-dark");
       card?.classList.remove("card-dark");
       localStorage.setItem("theme", "light");
     }
   }, [darkMode]);
 
   return (
+    <div className="top-logo">
+    <h1>devfinder</h1>
     <button onClick={() => setDarkMode((prev) => !prev)}>
-      {darkMode ? "Light Mode" : "Dark Mode"}
+      {darkMode ?  "DARK" : "LIGHT"}
+      {darkMode ?  <i className="fas fa-moon"></i> : sun_icon }
     </button>
+    </div>
   );
 };
 
