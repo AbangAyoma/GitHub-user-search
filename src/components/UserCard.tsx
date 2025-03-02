fd00c0a6a5f4c981a49870c610617be5dba77e2a
AbangAyoma <abangayoma@gmail.com>
2025-03-02 11:45:26 +0100

    feat: add when a user joined

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -15,18 +15,45 @@ interface UserCardProps {
   };
 }
 
+// const UserCard: React.FC<UserCardProps> = ({ user }) => {
+//   return (
+//     <div className="user-card">
+//       <img src={user.avatar_url} alt={user.login} />
+//       <h1>{user.name }</h1> 
+//       <p className="date">Joined: {FormatDate(user.created_at)}</p>
+//       <h3>{user.login}</h3>
+//       <p className="bio">{user.bio || "No bio available"}</p>
+//       <p>Followers: {user.followers}</p>
+//       <p>Repositories: {user.public_repos}</p>
+//     </div>
+//   );
+// };
+
+// export default UserCard;
+
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
   return (
+    <div className="position">
     <div className="user-card">
-      <img src={user.avatar_url} alt={user.login} />
-      <h1>{user.name }</h1> 
-      <p className="date">Joined: {FormatDate(user.created_at)}</p>
-      <h3>{user.login}</h3>
-      <p className="bio">{user.bio || "No bio available"}</p>
-      <p>Followers: {user.followers}</p>
-      <p>Repositories: {user.public_repos}</p>
+      {/* First row with image, name, and joined date */}
+      
+      <div className="top-row">
+        <img src={user.avatar_url} alt={user.login} />
+        <div className="details">
+          <h1>{user.name}</h1>
+          <p className="date">Joined: {FormatDate(user.created_at)}</p>
+        </div>
+      </div>
+
+      {/* Second row with bio, followers, and repos */}
+      <div className="bottom-row">
+        <h3>{user.login}</h3>
+        <p className="bio">{user.bio || "No bio available"}</p>
+        <p>Followers: {user.followers}</p>
+        <p>Repositories: {user.public_repos}</p>
+      </div>
+    </div>
     </div>
   );
 };
-
 export default UserCard;
