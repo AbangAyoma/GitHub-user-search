87cb6c9108caf09cf9e4413381fc833e24cfd622
abangayoma <abangatoma@gmail.com>
2025-03-07 14:55:49 +0100

    feat: add improved event handling and functionality to SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
-import './SearchBar.css';
+import "./SearchBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-
 
 // interface SearchBarProps {
 //   onSearch: (username: string) => void;
 // }
 
-const SearchBar: React.FC<{ onSearch: (username: string) => void }> = ({ onSearch }) => {
+const SearchBar: React.FC<{ onSearch: (username: string) => void }> = ({
+  onSearch,
+}) => {
   const [username, setUsername] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
 
   const handleSubmit = (e: React.FormEvent) => {
+    if (!username) return; // Prevent empty search
+    setIsSearching(true); // Set button to "Searching..."
     e.preventDefault(); // this will help Prevent page refresh
     if (username.trim()) {
       onSearch(username); // Callings the search function here
@@ -20,21 +24,24 @@ const SearchBar: React.FC<{ onSearch: (username: string) => void }> = ({ onSearc
 
   return (
     <div className="container">
-    <form onSubmit={handleSubmit} className="search-container">
-      <div className="search-cont">
-      <span><FontAwesomeIcon className="search-icon"
-        icon={faSearch}
-      /></span>
-       <input
-        type="text"
-        placeholder="Search GitHub username..."
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="search-input   rounded-md focus:outline-none"
-      />
-      </div>
-      <button type="submit" className="search-button">Search</button>
-    </form>
+      <form onSubmit={handleSubmit} className="search-container">
+        <div className="search-cont">
+          <span>
+            <FontAwesomeIcon className="search-icon" icon={faSearch} />
+          </span>
+          <input
+            type="text"
+            placeholder="Search GitHub username..."
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="search-input   rounded-md focus:outline-none"
+          />
+        </div>
+        <button type="submit" className="search-button">
+          {/* disabled={isSearching}   add this to the button tag to spoil your code */}
+          Search{/* {isSearching ? "Searching..." : "Search"} */}
+        </button>
+      </form>
     </div>
   );
 };
